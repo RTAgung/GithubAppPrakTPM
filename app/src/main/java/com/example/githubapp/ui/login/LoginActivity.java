2e4c19a2e1d68bcd98d442779b947d33169e8996
Rama Tri Agung <ramatriagung91@gmail.com>
2021-06-25 21:49:59 +0700

    nambah dikit

--- app/src/main/java/com/example/githubapp/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/githubapp/ui/login/LoginActivity.java
@@ -3,14 +3,23 @@ package com.example.githubapp.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.githubapp.R;
+import com.example.githubapp.ui.main.MainActivity;
 import com.example.githubapp.viewmodel.ViewModelFactory;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel viewModel;
+
+    private TextInputEditText etUsername, etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,22 @@ public class LoginActivity extends AppCompatActivity {
         ViewModelFactory factory = ViewModelFactory.getInstance(this);
         viewModel = new ViewModelProvider(this, factory).get(LoginViewModel.class);
 
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        btnLogin = findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(view -> {
+            String username = etUsername.getText().toString();
+            String password = etPassword.getText().toString();
+
+            if (username.isEmpty()) etUsername.setError("Cannot Empty");
+            else if (password.isEmpty()) etPassword.setError("Cannot Empty");
+            else {
+                if (viewModel.login(username, password) == 1)
+                    startActivity(new Intent(this, MainActivity.class));
+                else
+                    Toast.makeText(this, "Login Failed", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/githubapp/ui/main/MainActivity.java
+++ app/src/main/java/com/example/githubapp/ui/main/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.githubapp.ui.main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.githubapp.R;
+import com.example.githubapp.ui.favorite.FavoriteActivity;
+import com.example.githubapp.ui.login.LoginActivity;
 import com.example.githubapp.viewmodel.ViewModelFactory;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -33,12 +37,25 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.menu_home:
+                    setFragment(new HomeFragment());
                     break;
                 case R.id.menu_profile:
+                    setFragment(new ProfileFragment());
                     break;
             }
             return false;
         });
+
+        setFragment(new HomeFragment());
+    }
+
+    private void setFragment(Fragment fragment) {
+        if (fragment != null)
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frame, fragment)
+                    .show(fragment)
+                    .commit();
     }
 
     @Override
@@ -51,8 +68,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_favorite:
+                startActivity(new Intent(this, FavoriteActivity.class));
                 break;
             case R.id.menu_logout:
+                viewModel.logout();
+                Intent intent = new Intent(this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
                 break;
         }
         return super.onOptionsItemSelected(item);
