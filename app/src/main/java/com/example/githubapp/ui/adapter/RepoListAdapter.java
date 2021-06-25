6d733d62867533eea0cbb0edf0b18bd49f11219c
Rama Tri Agung <ramatriagung91@gmail.com>
2021-06-26 06:16:49 +0700

    nambah dikit

--- app/src/main/java/com/example/githubapp/ui/adapter/RepoListAdapter.java
+++ app/src/main/java/com/example/githubapp/ui/adapter/RepoListAdapter.java
@@ -1,4 +1,62 @@
 package com.example.githubapp.ui.adapter;
 
-public class RepoListAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.githubapp.R;
+import com.example.githubapp.data.model.UserGithub;
+import com.example.githubapp.data.model.UserReposGithub;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RepoListAdapter extends RecyclerView.Adapter<RepoListAdapter.ViewHolder> {
+    private List<UserReposGithub> reposList = new ArrayList<>();
+
+    public void setData(List<UserReposGithub> githubList) {
+        this.reposList.clear();
+        this.reposList.addAll(githubList);
+        notifyDataSetChanged();
+    }
+
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.repo_list, parent, false);
+        return new RepoListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+        holder.bind(reposList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return reposList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvName, tvLang, tvUpdate;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.det_nama);
+            tvLang = itemView.findViewById(R.id.det_bahasa);
+            tvUpdate = itemView.findViewById(R.id.det_update);
+        }
+
+        public void bind(UserReposGithub repos) {
+            tvName.setText(repos.getName());
+            tvLang.setText(repos.getLanguage());
+            tvUpdate.setText(repos.getUpdate());
+        }
+    }
 }
--- app/src/main/java/com/example/githubapp/ui/main/ProfileFragment.java
+++ app/src/main/java/com/example/githubapp/ui/main/ProfileFragment.java
@@ -1,5 +1,7 @@
 package com.example.githubapp.ui.main;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,14 +9,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.githubapp.R;
 import com.example.githubapp.data.model.UserGithub;
 import com.example.githubapp.data.model.UserReposGithub;
+import com.example.githubapp.ui.adapter.RepoListAdapter;
 import com.example.githubapp.viewmodel.ViewModelFactory;
 
 import org.jetbrains.annotations.NotNull;
@@ -25,8 +34,13 @@ public class ProfileFragment extends Fragment {
 
     private MainViewModel viewModel;
 
-    private UserGithub userGithub;
-    private List<UserReposGithub> userReposGithubList;
+    private TextView profileName, profileUsername, profileEmail;
+    private TextView profileDate, profileState, profileBio, followers, following, repos;
+    private ImageView profileAvatar;
+    private Button btnBrowser;
+    private RecyclerView rvRepoList;
+
+    private RepoListAdapter adapter;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -43,15 +57,56 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        rvRepoList = view.findViewById(R.id.rv_repo_list);
+        btnBrowser = view.findViewById(R.id.btn_browser);
+        profileAvatar = view.findViewById(R.id.profile_avatar);
+        repos = view.findViewById(R.id.repos_number);
+        following = view.findViewById(R.id.following_number);
+        followers = view.findViewById(R.id.followers_number);
+        profileBio = view.findViewById(R.id.profile_bio);
+        profileState = view.findViewById(R.id.profile_state);
+        profileDate = view.findViewById(R.id.profile_date);
+        profileEmail = view.findViewById(R.id.profile_email);
+        profileUsername = view.findViewById(R.id.profile_username);
+        profileName = view.findViewById(R.id.profile_name);
+
         ViewModelFactory factory = ViewModelFactory.getInstance(getContext());
         viewModel = new ViewModelProvider(requireActivity(), factory).get(MainViewModel.class);
 
-        viewModel.getDetailUser().observe(getViewLifecycleOwner(), userGithub -> {
-            this.userGithub = userGithub;
-        });
+        adapter = new RepoListAdapter();
+        rvRepoList.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        rvRepoList.setAdapter(adapter);
 
-        viewModel.getReposUser().observe(getViewLifecycleOwner(), userReposGithubs -> {
-            userReposGithubList = userReposGithubs;
+        viewModel.getDetailUser().observe(getViewLifecycleOwner(), this::populateProfile);
+
+        viewModel.getReposUser().observe(getViewLifecycleOwner(), this::populateRepo);
+    }
+
+    private void populateRepo(List<UserReposGithub> repos) {
+        adapter.setData(repos);
+    }
+
+    private void populateProfile(UserGithub github) {
+        profileName.setText(github.getName());
+        profileUsername.setText(github.getUsername());
+        profileEmail.setText(github.getEmail());
+        profileDate.setText(github.getCreateAt());
+        profileState.setText(github.getLocation());
+        profileBio.setText(github.getBio());
+        followers.setText(github.getFollowers() + "");
+        following.setText(github.getFollowing() + "");
+        repos.setText(github.getRepos() + "");
+
+        Glide.with(getView().getContext())
+                .load(github.getAvatar())
+                .centerCrop()
+                .into(profileAvatar);
+
+        btnBrowser.setOnClickListener(view -> {
+            String url = github.getLink();
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
         });
     }
 
