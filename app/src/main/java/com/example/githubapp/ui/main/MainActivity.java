ba20d45292561a27a0fdc79115311b98ad2d49a0
Rama Tri Agung <ramatriagung91@gmail.com>
2021-06-25 19:35:32 +0700

    nambah dikit

--- app/src/main/java/com/example/githubapp/ui/main/MainActivity.java
+++ app/src/main/java/com/example/githubapp/ui/main/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.githubapp.ui.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.githubapp.R;
 import com.example.githubapp.viewmodel.ViewModelFactory;
@@ -20,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
         ViewModelFactory factory = ViewModelFactory.getInstance(this);
         viewModel = new ViewModelProvider(this, factory).get(MainViewModel.class);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_favorite:
+                break;
+            case R.id.menu_logout:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
